7f9b9e7210d038e375e581b864496db01b79fe16
Paul Chambaz <paul.chambaz@tutanota.com>
2025-03-28 14:05:40 +0100

    added flake and gitignore

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        python = pkgs.python311;
+        pythonPackages = python.pkgs;
+        devPkgs = with pkgs; [
+          just
+          python
+        ];
+        pythonPkgs = with pythonPackages; [
+          numpy
+          matplotlib
+        ];
+      in {
+        app.default = {
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = devPkgs ++ pythonPkgs;
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
